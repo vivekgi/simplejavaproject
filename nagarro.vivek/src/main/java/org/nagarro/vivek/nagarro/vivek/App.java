703d277edb0b6d5b612fb15d96e5db6fc2a50f0f
vivek <vivek@vivek-VirtualBox>
2018-05-15 16:36:53 +0530

    maven commit

---
+++ nagarro.vivek/src/main/java/org/nagarro/vivek/nagarro/vivek/App.java
@@ -0,0 +1,13 @@
+package org.nagarro.vivek.nagarro.vivek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
